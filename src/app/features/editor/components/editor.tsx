e14e2666bc1132f77ba25960a7a14042ccb73ce6
Abhishek Sharma <sharma312006@gmail.com>
2025-10-27 10:41:58 +0530

    add node using react-workflow

--- src/app/features/editor/components/editor.tsx
+++ src/app/features/editor/components/editor.tsx
@@ -1,7 +1,28 @@
 'use client';
 
+import { useState, useCallback } from 'react';
+import {
+  ReactFlow,
+  applyNodeChanges,
+  applyEdgeChanges,
+  addEdge,
+  type Node,
+  type Edge,
+  NodeChange,
+  EdgeChange,
+  Connection,
+  Background,
+  Controls,
+  MiniMap,
+  Panel,
+} from '@xyflow/react';
+
 import { ErrorView, LoadingView } from '@/components/entity-views';
 import { useSuspenseWorkflow } from '../../workflows/hooks/use-workflows';
+
+import '@xyflow/react/dist/style.css';
+import { nodeComponents } from '@/config/node-components';
+import { AddNodeButton } from './add-node-button';
 
 export const EditorLoading = () => {
   return <LoadingView message="Loading editor" />;
@@ -14,5 +35,43 @@ export const EditorError = () => {
 export const Editor = ({ workflowId }: { workflowId: string }) => {
   const { data: workflow } = useSuspenseWorkflow(workflowId);
 
-  return <p>{JSON.stringify(workflow, null, 2)}</p>;
+  const [nodes, setNodes] = useState<Node[]>(workflow.nodes);
+  const [edges, setEdges] = useState<Edge[]>(workflow.edges);
+
+  const onNodesChange = useCallback(
+    (changes: NodeChange<Node>[]) =>
+      setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)),
+    [],
+  );
+  const onEdgesChange = useCallback(
+    (changes: EdgeChange<Edge>[]) =>
+      setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)),
+    [],
+  );
+  const onConnect = useCallback(
+    (params: Connection) =>
+      setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot)),
+    [],
+  );
+
+  return (
+    <div className="size-full">
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        nodeTypes={nodeComponents}
+        fitView
+      >
+        <Background />
+        <Controls />
+        <MiniMap />
+        <Panel position="top-right">
+          <AddNodeButton />
+        </Panel>
+      </ReactFlow>
+    </div>
+  );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,12 @@
+'use client';
+import { useTRPC } from '@/trpc/client';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { Button } from '@/components/ui/button';
+import { requireUnAuth } from '@/lib/auth-utils';
+import { toast } from 'sonner';
 
-"use client"
-import {useTRPC} from "@/trpc/client";
-import {useMutation, useQuery, useQueryClient} from "@tanstack/react-query";
-import {Button} from "@/components/ui/button";
-import {requireUnAuth} from "@/lib/auth-utils";
-import {toast} from "sonner";
-
-const Page =  () => {
-
-    const trpc = useTRPC()
-    const queryClient = useQueryClient()
-    const {data} = useQuery(trpc.getWorkflows.queryOptions())
-
-    const create = useMutation(trpc.createWorkflows.mutationOptions({
-        onSuccess: () => {
-            queryClient.invalidateQueries(trpc.getWorkflows.queryOptions())
-        }
-    }))
-
-    const testAi = useMutation(trpc.testAi.mutationOptions({
-        onSuccess: () => {
-            toast.success("Job queue successfully")
-        }
-    }))
-
-  return (
-    <div className="min-h-screen min-w-screen flex items-center justify-center">
-        <div>
-            {JSON.stringify(data, null, 2)}
-        </div>
-       <Button disabled={testAi.isPending} onClick={()=>testAi.mutate()}>Test Ai</Button>
-        <Button disabled={create.isPending} onClick={()=>create.mutate()}>Check Workflow</Button>
-    </div>
-  );
+const Page = () => {
+  return <></>;
 };
 
 export default Page;
