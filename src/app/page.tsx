ed8e809ba239b34acf69ddca81ac1f2fc69ce35f
Abhishek Sharma <sharma312006@gmail.com>
2025-12-06 14:34:07 +0530

    change redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
 'use client';
-import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
+
 
 const Page = () => {
-  const router = useRouter();
-  useEffect(() => {
-    router.push('/workflows');
-  }, []);
+
   return <></>;
 };
 
-export default Page;
+export default Page
